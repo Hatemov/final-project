d4bf149225a964e55422a02e31cd4f5d7a16e7da
Hatemov <emin.hetemli206@gmail.com>
2023-10-08 14:21:36 +0400

    Final

---
+++ index.js
@@ -0,0 +1,19 @@
+let line = document.querySelector(".line")
+let cancel = document.querySelector(".cancel")
+let menu = document.querySelector(".menu")
+
+menu.addEventListener("click",()=>{
+    line.style.display = "block"
+    menu.style.visibility = "hidden"
+})
+cancel.addEventListener("click",()=>{
+    line.style.display = "none"
+menu.style.visibility = "visible"
+})
+
+let swiper = new Swiper(".mySwiper", {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
